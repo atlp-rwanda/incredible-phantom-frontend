395064a750aeb764992b8086113b02be05bfaee2
KundwaBruno <kundwabruno@gmail.com>
2020-12-28 20:52:20 +0200

    chore(setup redux):setup redux and creating a counter for testing
    chore(test):set jest for testing
    
    this PR serves to configure testing environment, github actions and codeclimate
    
    [finishes BjQmh9rH]
    
    solve failing tests
    
    bug
    
    solve failing tests
    
    bug
    
    solve renaming file
    
    renaming from tests.test.js to welcome.test.js
    
    bug
    
    chore:set up continuous integration
    
    setting testing environment as jest and enzyme, codeclimate,github actions and heroku deployment
    
    BjQmh9rH
    
     solving import issue
    
     solving tracked files issue
    
    chore(redux setup):setup redux and creating a counter component for testing

--- src/index.js
+++ src/index.js
@@ -2,5 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.scss';
 import App from './router/App';
+import { createStore, applyMiddleware, compose } from 'redux';
+import rootReducer from './reducers/store';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__(),
+  ),
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+
+  document.getElementById('root'),
+);
---
+++ src/reducers/store.js
@@ -0,0 +1,5 @@
+import { combineReducers } from 'redux';
+import { counterReducer } from './counterReducer';
+
+const rootReducers = combineReducers({ counterReducer });
+export default rootReducers;
