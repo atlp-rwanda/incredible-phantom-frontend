32953e658d19deee5a70bc02673c355497491a93
KundwaBruno <kundwabruno@gmail.com>
2020-12-28 20:52:20 +0200

    chore(setup redux):setup redux and creating a counter for testing
    chore(test):set jest for testing
    
    this PR serves to configure testing environment, github actions and codeclimate
    
    [finishes BjQmh9rH]
    
    solve failing tests
    
    bug
    
    solve failing tests
    
    bug
    
    solve renaming file
    
    renaming from tests.test.js to welcome.test.js
    
    bug
    
    chore:set up continuous integration
    
    setting testing environment as jest and enzyme, codeclimate,github actions and heroku deployment
    
    BjQmh9rH
    
     solving import issue
    
     solving tracked files issue
    
    chore(redux setup):setup redux and creating a counter component for testing

--- src/router/App.js
+++ src/router/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import LandingPage from '../pages/LandingPage';
 import error from '../components/NotFound';
-
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import Counter from '../components/Counter';
+
 const App = () => {
   return (
     <Router>
       <Switch>
         <Route path='/' exact component={LandingPage} />
+        <Route path='/counter' component={Counter} />
         <Route component={error} />
       </Switch>
     </Router>
