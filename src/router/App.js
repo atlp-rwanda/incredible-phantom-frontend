c2d528d9a60e9fb1f762f33523ab3dd54be9684b
mansurissa <mansurissa6@gmail.com>
2021-01-11 18:14:36 +0200

    rebasing task of ci/cd

--- src/router/App.js
+++ src/router/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { LandingPage } from '../pages/landingPage';
-import { error } from '../componets/NotFound';
+import LandingPage from '../pages/landingPage';
+import error from '../components/NotFound';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
-import Counter from '../componets/Counter';
+import Counter from '../components/Counter';
 
 const App = () => {
   return (
