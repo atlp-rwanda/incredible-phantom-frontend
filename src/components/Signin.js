e48f92d6ad590f5078415b11a4ee4dd543a2b405
mansurissa <mansurissa6@gmail.com>
2021-02-19 11:27:09 +0200

    changing landing page on dashboard from overview to drivers

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -48,7 +48,7 @@ const LoginForm = ({ submitHandler, error, details, setDetails }) => {
                 }
                 value={details.password}
               />
-              <Link to='/' className='link'>
+              <Link to='forgotpassword' className='link'>
                 {t('signin.forgotPassword')}
               </Link>
             </div>
